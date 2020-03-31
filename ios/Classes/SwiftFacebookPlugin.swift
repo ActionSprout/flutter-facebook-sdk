85cd974bffec87524df6ee0a718f4d182e87e188
Michael Schoonmaker <hello@schoonology.com>
2020-03-31 15:04:54 -0700

    Facebook: Swiftlint.

--- ios/Classes/SwiftFacebookPlugin.swift
+++ ios/Classes/SwiftFacebookPlugin.swift
@@ -1,14 +1,14 @@
 import Flutter
 import UIKit
 
-public class SwiftFacebookPlugin: NSObject, FlutterPlugin {
-  public static func register(with registrar: FlutterPluginRegistrar) {
-    let channel = FlutterMethodChannel(name: "facebook", binaryMessenger: registrar.messenger())
-    let instance = SwiftFacebookPlugin()
-    registrar.addMethodCallDelegate(instance, channel: channel)
-  }
+public class SwiftFacebookPlugin: NSObject, FlutterPlugin, FlutterApplicationLifeCycleDelegate {
+    public static func register(with registrar: FlutterPluginRegistrar) {
+        let channel = FlutterMethodChannel(name: "actionsprout.com/facebook", binaryMessenger: registrar.messenger())
+        let instance = SwiftFacebookPlugin()
+        registrar.addMethodCallDelegate(instance, channel: channel)
+    }
 
-  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-    result("iOS " + UIDevice.current.systemVersion)
-  }
+    public func handle(_: FlutterMethodCall, result: @escaping FlutterResult) {
+        result("iOS " + UIDevice.current.systemVersion)
+    }
 }
