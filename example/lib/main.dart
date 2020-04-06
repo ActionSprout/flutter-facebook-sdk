4ea17a824bf1f6899c7e3bc6575cc5ecf220bee1
Michael Schoonmaker <hello@schoonology.com>
2020-04-06 11:16:19 -0700

    Facebook: Wire up login action.
    This includes some preliminary work to surface errors, which required
    redefining some constants that were not exported by the iOS SDK.

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,8 +1,5 @@
-import 'package:flutter/material.dart';
-import 'dart:async';
-
-import 'package:flutter/services.dart';
 import 'package:facebook/facebook.dart';
+import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
@@ -12,43 +9,24 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
-  String _platformVersion = 'Unknown';
-
-  @override
-  void initState() {
-    super.initState();
-    initPlatformState();
-  }
-
-  // Platform messages are asynchronous, so we initialize in an async method.
-  Future<void> initPlatformState() async {
-    String platformVersion;
-    // Platform messages may fail, so we use a try/catch PlatformException.
-    try {
-      platformVersion = await Facebook.platformVersion;
-    } on PlatformException {
-      platformVersion = 'Failed to get platform version.';
-    }
-
-    // If the widget was removed from the tree while the asynchronous platform
-    // message was in flight, we want to discard the reply rather than calling
-    // setState to update our non-existent appearance.
-    if (!mounted) return;
-
-    setState(() {
-      _platformVersion = platformVersion;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Plugin example app'),
+          title: const Text('Facebook example'),
         ),
         body: Center(
-          child: Text('Running on: $_platformVersion\n'),
+          child: RaisedButton(
+            onPressed: () async {
+              try {
+                await Facebook.logIn();
+              } on Object catch (error) {
+                print('Login failed with: $error');
+              }
+            },
+            child: const Text('Login with Facebook'),
+          ),
         ),
       ),
     );
--- ios/Classes/SwiftFacebookPlugin.swift
+++ ios/Classes/SwiftFacebookPlugin.swift
@@ -3,6 +3,23 @@ import FacebookLogin
 import Flutter
 import UIKit
 
+let kFBSDKErrorDeveloperMessageKey = "com.facebook.sdk:FBSDKErrorDeveloperMessageKey"
+
+func format(error: Error) -> FlutterError {
+    let error = error as NSError
+    var message: String?
+
+    if let devMessage = error.userInfo[kFBSDKErrorDeveloperMessageKey] as? String? {
+        message = devMessage
+    }
+
+    return FlutterError(
+        code: String(describing: error.code),
+        message: message,
+        details: nil
+    )
+}
+
 public class SwiftFacebookPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
         let channel = FlutterMethodChannel(name: "actionsprout.com/facebook", binaryMessenger: registrar.messenger())
@@ -18,9 +35,18 @@ public class SwiftFacebookPlugin: NSObject, FlutterPlugin {
     }
 
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+        guard let arguments = call.arguments as? [String: Any?] else {
+            result(FlutterError(
+                code: "INVALID_ARGS",
+                message: "Arguments must be a Map<String, dynamic>",
+                details: nil
+            ))
+            return
+        }
+
         switch call.method {
-        case "getPlatformVersion":
-            result("iOS " + UIDevice.current.systemVersion)
+        case "login":
+            loginWithFacebook(arguments, result)
         default:
             result(FlutterError(
                 code: "UNIMPLEMENTED",
@@ -37,5 +63,33 @@ public class SwiftFacebookPlugin: NSObject, FlutterPlugin {
 
     public func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]) -> Bool {
         return ApplicationDelegate.shared.application(app, open: url, options: options)
+    }
+
+    private func loginWithFacebook(_ args: [String: Any?], _ result: @escaping FlutterResult) {
+        guard let permissionNames = args["permissions"] as? [String] else {
+            result(FlutterError(
+                code: "INVALID_ARG",
+                message: "Method 'login' requires permissions as a list of Strings.",
+                details: nil
+            ))
+            return
+        }
+
+        let permissions = permissionNames.map { name in
+            Permission(stringLiteral: name)
+        }
+
+        facebookLoginManager.logIn(
+            permissions: permissions
+        ) { loginResult in
+            switch loginResult {
+            case .cancelled:
+                print("Login cancelled: \(loginResult)")
+            case .success(granted: _, declined: _, token: _):
+                print("Login succeeded: \(loginResult)")
+            case let .failed(error):
+                result(format(error: error))
+            }
+        }
     }
 }
